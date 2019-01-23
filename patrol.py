649cc01724b8a03863ac55ddd3e70b6c0bbe59cb
daghaian <16483722+daghaian@users.noreply.github.com>
2019-01-22 20:00:31 -0800

    Added Kick 4K Transcodes logic  (#19)
    * Added configuration options for 4K Transcode Kicking
    
    * Added logic for kicking 4K Transcodes
    
    * Attempt to grab quality of stream

--- patrol.py
+++ patrol.py
@@ -98,6 +98,8 @@ def should_kick_stream(stream):
             return True, 0, config.KICK_TRANSCODE_MESSAGE
         if stream.state == 'paused' and config.KICK_PAUSED_TRANSCODES:
             return True, config.KICK_PAUSED_GRACE_MINS, config.KICK_PAUSED_MESSAGE
+        if stream.quality == '4K' and config.KICK_4K_TRANSCODE:
+            return True, 0, config.KICK_4K_TRANSCODE_MESSAGE
 
     else:
         # stream is directplay - check specifics
